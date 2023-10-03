339824cba75d0aa68c2c85118fbb872d568888f0
João Falcão <jgabfalcao@gmail.com>
2023-10-03 12:19:37 -0300

    feat: Inserting database connection

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
+
+const routes = require("./routes/TaskRoute")
 
 const cors = require("cors");
 
@@ -11,8 +13,14 @@ const PORT = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected..."))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+
+  app.use("/api", routes);
+
+
 
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
