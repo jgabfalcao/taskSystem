bba668972f483f8b2ce8ac52c03ecb781512960b
João Falcão <jgabfalcao@gmail.com>
2023-10-03 14:27:10 -0300

    feat: List component with map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import axios from "axios";
+import { baseURL } from "./utils/constant";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [updateUI, setUpdateUI] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
 
-export default App
+  useEffect(() => {
+    axios.get(`${baseURL}/get`).then((res) => {
+      console.log(res.data);
+      setTasks(res.data);
+    });
+  }, [updateUI]);
+
+  const addTask = () => {
+    axios.post(`${baseURL}/save`, { task: input }).then((res) => {
+      console.log(res.data);
+      setInput("");
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  const updateMode = (id, text) => {
+    console.log(text);
+    setInput(text);
+    setUpdateId(id);
+  };
+
+  const updateTask = (id, text) => {
+    axios.put(`${baseURL}/update/${updateId}`, { task: input }).then((res) => {
+      console.log(res.data);
+      setUpdateUI((prevState) => !prevState);
+      setUpdateId(null);
+      setInput("");
+    });
+  };
+
+  return (
+    <main>
+      <h1 className="title">TASKSYSTEM</h1>
+
+      <div className="input_holder">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button type="submit" onClick={updateId ? updateTask : addTask}>
+          {updateId ? "Update Task" : "Add Task"}
+        </button>
+      </div>
+
+      <ul>
+        {tasks.map((task) => (
+          <List
+            key={task.id}
+            id={task._id}
+            task={task.task}
+            setUpdateUI={setUpdateUI}
+            updateMode={updateMode}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default App;
